87dbd2f5fa342557d3282450c2003f188015ca81
JeanJacques4269 <45049767+JeanJacques4269@users.noreply.github.com>
2021-05-29 00:30:50 +0200

    Pas ouf hein

--- constants.py
+++ constants.py
@@ -1,13 +1,19 @@
 from win32api import GetSystemMetrics
+import pygame
 
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RANDOM = (145, 32, 14)
+RANDOM2 = (45, 32, 74)
 
 BG_COLOR = BLACK
+CASECOLOR1 = WHITE
+CASECOLOR2 = BLACK
 
 # Sizes
-WIDTH = GetSystemMetrics(0)
-HEIGHT = GetSystemMetrics(1)
+WIDTH, HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1)
 MIDW = WIDTH // 2
 MIDH = HEIGHT // 2
+BOARDSIZE = HEIGHT * 0.8
+
