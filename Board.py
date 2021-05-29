42fd0e1d7269f05798ce29f72a2b85ee65c6a2fe
JeanJacques4269 <45049767+JeanJacques4269@users.noreply.github.com>
2021-05-29 23:19:46 +0200

    Pieces showing on screen :)

--- Board.py
+++ Board.py
@@ -5,9 +5,10 @@ import pygame
 class Board:
     def __init__(self, logic, size):
         self.size = size
-        self.case_size = self.size // 8
+        self.case_size = int(self.size // 8)
 
-        self.color1 = CASECOLOR1
+        self.x = 0
+        self.y = 0
 
         self.logic = logic
 
@@ -17,6 +18,8 @@ class Board:
         self.draw_pieces(win)
 
     def draw_board(self, win, x, y):
+        self.x = x
+        self.y = y
         for i in range(8):
             for j in range(8):
                 if (i + j) % 2 == 0:
@@ -28,5 +31,10 @@ class Board:
                                  (x + self.case_size * i, y + self.case_size * j, self.case_size, self.case_size))
 
     def draw_pieces(self, win):
-        pass
-
+        board = self.logic.board
+        for i in range(8):
+            for j in range(8):
+                if board[i][j].strip() != "":
+                    image = globals()[f"{board[i][j]}_image"]
+                    image = pygame.transform.smoothscale(image, (self.case_size, self.case_size))
+                    win.blit(image, (self.x + self.case_size * j, self.y + self.case_size * i))
--- constants.py
+++ constants.py
@@ -16,3 +16,20 @@ WIDTH, HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1)
 MIDW = WIDTH // 2
 MIDH = HEIGHT // 2
 BOARDSIZE = HEIGHT * 0.8
+
+# Pieces
+wp_image = pygame.image.load(r"assets/row-1-col-6.png")
+wr_image = pygame.image.load(r"assets/row-1-col-5.png")
+wn_image = pygame.image.load(r"assets/row-1-col-4.png")
+wb_image = pygame.image.load(r"assets/row-1-col-3.png")
+wq_image = pygame.image.load(r"assets/row-1-col-2.png")
+wk_image = pygame.image.load(r"assets/row-1-col-1.png")
+
+bp_image = pygame.image.load(r"assets/row-2-col-6.png")
+br_image = pygame.image.load(r"assets/row-2-col-5.png")
+bn_image = pygame.image.load(r"assets/row-2-col-4.png")
+bb_image = pygame.image.load(r"assets/row-2-col-3.png")
+bq_image = pygame.image.load(r"assets/row-2-col-2.png")
+bk_image = pygame.image.load(r"assets/row-2-col-1.png")
+
+
