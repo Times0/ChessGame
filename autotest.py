3e1b0919efc1a19a5a090b4acebd84d33793b780
JeanJacques4269 <spotopaz@gmail.com>
2021-08-03 14:53:06 +0200

    Improved tests

--- autotest.py
+++ autotest.py
@@ -1,9 +1,11 @@
+import time
+
 import Logic
 import bot
 from constants import *
 
 
-def mate_test(fenmates: list):
+def mate_test(fenmates: list, name):
     c = 0
     for i, fen in enumerate(fenmates):
         a = fen.split(" ")
@@ -12,20 +14,22 @@ def mate_test(fenmates: list):
         virtual = Logic.Logic(fen=fen)
 
         print(f"Testing fenmate[{i}]...", end="")
-        eval, move = bott.play_well(virtual, 2)
 
+        start = time.time()
+        eval, move = bott.play_well(virtual, 2)
+        end = time.time()
         if abs(eval) < 500:
             print(f"Mat non trouvé pour fenmate2[{i}], {eval=}")
             c += 1
         else:
-            print(f"Mat trouvé")
+            print(f"Mat trouvé en {end - start:.2f} s")
 
-    print(f"Tests de mat terminés pour {fenmates}, {c} tests ont échoués sur {len(fenmates)}")
+    print(f"Tests de mat terminés pour {name}, {c} tests ont échoués sur {len(fenmates)}")
 
 
 def main():
-    mate_test(fenmate2)
-    mate_test(fenmate3)
+    mate_test(fenmate2, "fenmate2")
+    mate_test(fenmate3, "fenmate3")
 
 
 if __name__ == "__main__":
